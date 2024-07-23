688a8853e71f1c861c80240a789e44244229e0ed
selfishAltruism <officialkyus@gmail.com>
2024-07-24 02:18:57 +0900

    feat: set state in sign up progress (#7)

--- src/pages/auth/signup/SignUpPage.tsx
+++ src/pages/auth/signup/SignUpPage.tsx
@@ -1,12 +1,17 @@
-import * as Styles from "./Styles";
-
+import { useState } from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
 
+import { Button } from "@/entities";
+
+import * as Styles from "./Styles";
+
 const SignUpPage = () => {
+  const [progress, setProgress] = useState(1);
+
   return (
     <>
       <ProgressBar
-        completed={1}
+        completed={progress}
         maxCompleted={3}
         labelColor="#6482EB"
         bgColor="#6482EB"
@@ -14,6 +19,13 @@ const SignUpPage = () => {
         borderRadius="3px"
       />
       SingUp
+      <Button
+        onClick={() => {
+          setProgress(progress + 1);
+        }}
+      >
+        {progress !== 3 ? "계속하기" : "가입하기"}
+      </Button>
     </>
   );
 };
