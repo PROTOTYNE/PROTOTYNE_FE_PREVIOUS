58830a9b54a09bc37e60664ad0e78460b1d77791
selfishAltruism <officialkyus@gmail.com>
2024-07-28 20:49:32 +0900

    chore: add user info type (#7)

--- src/shared/@types/user.d.ts
+++ src/shared/@types/user.d.ts
@@ -5,8 +5,55 @@ declare namespace User {
     password: string;
   }
 
-  //Store
-  export interface UserStore {
-    isSignIn: boolean;
+  export interface BasicInfo {
+    name: string;
+    birthYear: string;
+    birthMonth: string;
+    birthDay: string;
+    gender: "MALE" | "FEMALE";
+    familyNum: number;
   }
+
+  export interface AdditionalInfo {
+    occupation:
+      | "STUDENT"
+      | "OFFICE"
+      | "PROFESSIONAL"
+      | "SELFEMPLOYED"
+      | "OTHER"
+      | undefined;
+    income: 2000 | 4000 | 6000 | 8000 | 9999 | undefined;
+    interests:
+      | "FITNESS"
+      | "TRAVEL"
+      | "READING&MOVIES"
+      | "COOKING"
+      | "GAMES"
+      | undefined;
+    familyComposition:
+      | 1
+      | "COUPLE"
+      | "COUPLE&CHILDREN"
+      | "PARENTS&CHILDREN"
+      | "EXTENDFAMILY"
+      | undefined;
+    productType: (
+      | "ELECTRONIC"
+      | "FASHION&BEAUTY"
+      | "FOOD"
+      | "HOUSEHOLD"
+      | "HEALTH"
+    )[];
+    phone: (
+      | "SMARTPHONE1"
+      | "SMARTPHONE2"
+      | "SMARTPHONE9"
+      | "TABLET"
+      | "SMARTWATCH"
+    )[];
+    healthStatus: 1 | 2 | 3 | 4 | 5 | undefined;
+  }
+
+  //Store
+  export interface UserStore {}
 }
