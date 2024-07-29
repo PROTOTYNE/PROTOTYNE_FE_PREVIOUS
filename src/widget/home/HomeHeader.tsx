c602b5afefa0d7c893497081a0dc5be50cf741bb
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-07-29 23:35:46 +0900

    feat: modify home header

--- src/widget/home/HomeHeader.tsx
+++ src/widget/home/HomeHeader.tsx
@@ -1,6 +1,7 @@
 import { Logo, Bookmark } from '@/entities';
 import { useNavigate } from 'react-router-dom';
 import styled from "@emotion/styled";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 
 const IconContainer = styled.div`
   display: flex;
@@ -11,22 +12,23 @@ const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  background: linear-gradient(to right, #3C435A 0%, #313131 51%, #937A6D 100%);
+  background: linear-gradient(to right, #7995B2 0%, #476090 51%, #0D1B4A 100%);
 `;
-
-
+const Menu = styled(MenuRoundedIcon)`
+    color: white;
+    font-size: 2rem;
+    margin: 5px 0px;
+`;
 export const HomeHeader = () => {
     const navigate = useNavigate();
     return (
         <HeaderContainer>
             <IconContainer>
-                <Bookmark src="./image/bookmark.svg" position="none" top="0%" left="0%" />
-                <img src="./image/alarm.svg" alt="alarm" />
+                <Menu />
             </IconContainer>
             <Logo src="./image/logo2.png" alt="logo"/>
             <IconContainer>
                 <img src="./image/search.svg" alt='search' onClick={ () => navigate("/search") } />
-                <img src="./image/account.svg" alt='search' />
             </IconContainer>
         </HeaderContainer>
     );
