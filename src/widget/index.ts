737fe603d185a382721f754fa90e480aabba392d
이혜성 <hyeseong@ihyeseong-ui-MacBookAir.local>
2024-08-09 19:52:02 +0900

    Week 3-(3)

--- src/widget/index.ts
+++ src/widget/index.ts
@@ -1,3 +1,4 @@
+// index.ts
 export { HomeHeader } from "./home/HomeHeader";
 export { Categories } from "./home/Categories";
 export { Prototypes, BookmarkPrototypes } from "./home/Prototypes";
@@ -8,3 +9,6 @@ export { ProductExperience } from "./mypage/ProductExperience";
 export { ProductInfo } from "./mypage/ProductInfo";
 export { UserInfoWidget } from "./mypage/UserInfo";
 export { WishList } from "./mypage/WishList";
+
+// 경로 수정
+export { ProductInfoContainer } from "../widget/mypage/ProductInfoContainer"; // src/widget/mypage/ProductInfoContainer.tsx의 올바른 상대 경로
