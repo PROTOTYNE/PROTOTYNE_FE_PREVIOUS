82c46b973e5892283ab8ce5103a7cc6efdfae1cc
selfishAltruism <officialkyus@gmail.com>
2024-08-09 16:52:32 +0900

    fix: header (#4)

--- src/entities/element/Header.tsx
+++ src/entities/element/Header.tsx
@@ -1,41 +1,8 @@
 import styled from "@emotion/styled";
-import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNewRounded'
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
-const BackIcon = styled(ArrowBackIosNewRoundedIcon)`
-    color: white;
-    margin: 16px 15px;
-    position: fixed;
-    top: 0px;
-    left: 0px;
-`;
-const HeaderContainer = styled.header`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    background: linear-gradient(to right, #7995B2 0%, #476090 51%, #0D1B4A 100%);
-`;
-const Title = styled.div`
-    color: white;
-    font-size: 25px;
-    font-weight: bold;
-    margin: 16px 0px;
-`;
-export const Header = () => {
-    const navigate = useNavigate();
-
-    return (
-        <HeaderContainer>
-            <BackIcon onClick={() => {navigate('/home')}}/>
-            <Title>
-                관심 목록
-            </Title>
-        </HeaderContainer>
-    );
-}
-
-export const HomeHeader = ({
+export const Header = ({
   children,
   onBack,
   background,
@@ -115,6 +82,8 @@ const StyledContioner = styled.div`
   left: 0px;
 
   color: white;
+
+  z-index: 10;
 `;
 
 const BackButton = styled(ArrowBackIosNewIcon)`
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,12 +1,20 @@
-
-
 export { Container, MidContainer, SearchP } from "./element/Container";
-export { LargeImage, BottomImage, Logo, PrototypeImg, Bookmark } from "./element/Image";
-export { Prototype, MiniPrototype, BookmarkPrototype } from "./element/Prototype";
+export {
+  LargeImage,
+  BottomImage,
+  Logo,
+  PrototypeImg,
+  Bookmark,
+} from "./element/Image";
+export {
+  Prototype,
+  MiniPrototype,
+  BookmarkPrototype,
+} from "./element/Prototype";
 export { Button, DisableButton, TwoOptionsButton } from "./element/Button";
 export { MidPointLine, GrayLine } from "./element/Line";
 export { Sidebar } from "./element/Sidebar";
-export { Header, HomeHeader } from "./element/Header";
+export { Header } from "./element/Header";
 export { SelectInput } from "./element/SelectInput";
 export { MultiSelectInput } from "./element/MultiSelectInput";
 
